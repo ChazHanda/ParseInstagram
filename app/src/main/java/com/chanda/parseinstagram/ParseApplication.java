cd9e1053f33e386ebde85923b2f469c4d1fd8a5a
ChazHanda <ChazHanda@gmail.com>
2021-02-28 22:37:06 -0800

    Added the ability to take pictures Added posting

--- app/src/main/java/com/chanda/parseinstagram/ParseApplication.java
+++ app/src/main/java/com/chanda/parseinstagram/ParseApplication.java
@@ -3,12 +3,15 @@ package com.chanda.parseinstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("CKFh3Q22aIoVRXzY8K880CoZsHUBYeHU0BA3Wgv1")
