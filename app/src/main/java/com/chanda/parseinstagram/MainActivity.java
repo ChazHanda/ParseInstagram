63c905c44e01e6a689cd716b108371f8a314fd30
ChazHanda <ChazHanda@gmail.com>
2021-03-09 22:52:24 -0800

    Can see posts with PostsAdapter

--- app/src/main/java/com/chanda/parseinstagram/MainActivity.java
+++ app/src/main/java/com/chanda/parseinstagram/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -22,6 +23,8 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.chanda.parseinstagram.fragments.ComposeFragment;
+import com.chanda.parseinstagram.fragments.PostsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -38,6 +41,7 @@ import static com.parse.ParseUser.getCurrentUser;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
+    final FragmentManager fragmentManager = getSupportFragmentManager();
     private BottomNavigationView bottomNavigationView;
 
 
@@ -55,23 +59,24 @@ public class MainActivity extends AppCompatActivity {
                 switch (menuItem.getItemId()) {
                     case R.id.action_home:
                         Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
-                        //fragment = fragment1;
+                        fragment = new PostsFragment();
                         break;
                     case R.id.action_compose:
                         Toast.makeText(MainActivity.this, "Compose", Toast.LENGTH_SHORT).show();
-                       //fragment = fragment2;
+                        fragment = new ComposeFragment();
                         break;
                     case R.id.action_profile:
                     default:
                         Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
-                        //fragment = fragment3;
+                        fragment = new ComposeFragment();
                         break;
                 }
-                //fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
         });
-
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
 
 
     }
